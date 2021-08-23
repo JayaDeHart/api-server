564fa5db3c52a8143d4f64664c2fe685fe1ff326
Jaya DeHart <dehartj97@gmail.com>
2021-08-22 22:33:56 -0700

    ready for submission

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,23 @@
 "use strict";
+const POSTGRES_URI =
+  process.env.NODE_ENV === "test" ? "sqlite:memory" : process.env.DATABASE_URL;
+let sequelizeOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+    : {};
 const { Sequelize, DataTypes } = require("sequelize");
 const gamesSchema = require("./games.js");
 const booksSchema = require("./books.js");
 const collectionClass = require("./collection-class");
+let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
+
 
 //schema -> model -> class
 const booksModel = booksSchema(sequelize, DataTypes);
@@ -11,22 +26,10 @@ const gamesModel = gamesSchema(sequelize, DataTypes);
 const booksCollection = new collectionClass(booksModel);
 const gamesCollection = new collectionClass(gamesModel);
 
-const POSTGRES_URI =
-  process.env.NODE_ENV === "test" ? "sqlite:memory" : process.env.DATABASE_URL;
 
-let sequelizeOptions =
-  process.env.NODE_ENV === "production"
-    ? {
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      }
-    : {};
 
-let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
+
+
 
 module.exports = {
   db: sequelize,
